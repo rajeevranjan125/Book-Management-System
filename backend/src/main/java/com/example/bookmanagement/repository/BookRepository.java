7518007be1867b934500415e5d745ff18358474e
Rajeev Prasad <rajeev.ranjan.prasad1@ibm.com>
2026-07-24 23:47:36 +0530

    added findById function call

--- backend/src/main/java/com/example/bookmanagement/repository/BookRepository.java
+++ backend/src/main/java/com/example/bookmanagement/repository/BookRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByTitle(String title);
+
+    Book findById(Long id);
 }
